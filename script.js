986f68bf1496e88928081dccf259a4cb2995bc45
Rodrigo-I2019 <rd7435935@gmail.com>
2025-10-17 10:40:44 -0500

    Versión inicial de mi página web

---
+++ script.js
@@ -0,0 +1,7 @@
+function saludar(){
+alert("¡Hola desde JavaScript!");
+}
+function agradecer(){
+alert("Naci para quedarme okey");
+alert("Quiero decirte que los mejores momentos que tuve, son cuando creo algo por mi mismo");
+}
