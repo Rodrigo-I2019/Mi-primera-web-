2806254b2184911a8fb19b9765679bc09a00184b
Rodrigo-I2019 <rd7435935@gmail.com>
2025-10-18 19:18:12 -0500

    Actualización de estilos, index y script

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function agradecer(){
 alert("Naci para quedarme okey");
 alert("Quiero decirte que los mejores momentos que tuve, son cuando creo algo por mi mismo");
 }
+function despedir(){
+alert("Gracias por ver mi pagina")
+alert("Espero que nos volvamos a ver")
+}
